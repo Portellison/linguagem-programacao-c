9023764d0bd3e66fd4f055b11f27c7461ed4a42b
Portellison <pedroportellafaria@hotmail.com>
2025-05-28 20:48:56 -0300

    Update ex1.c

--- aula15/ex1.c
+++ aula15/ex1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    FILE *quadrado = fopen("quadrado.svg", "w");
+
+    fprintf(quadrado, "<svg version=\"1.1\"\nwidth=\"300\" height=\"200\"\nxmlns=\"http://www.w3.org/2000/svg\">\n<rect x=\"20\" y=\"30\" width=\"100\" height=\"100\" fill=\"red\" />\n</svg>");
+
+    fclose(quadrado);
+    
+    return 0;
+}
 
